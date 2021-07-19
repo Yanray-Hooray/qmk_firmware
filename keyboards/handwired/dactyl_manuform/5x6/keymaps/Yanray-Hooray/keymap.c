a76adaf3b02bc2730962c75fc188fa275aedefe2
Ryan Royals <ryan.royals95@gmail.com>
2021-07-19 17:58:29 +0930

    moved arrow keys to line up with VIM

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/Yanray-Hooray/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/Yanray-Hooray/keymap.c
@@ -19,13 +19,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_LALT,KC_GRV,                                                          KC_LBRC,KC_RBRC,
                                                 KC_LSFT,KC_SPC ,         KC_BSPC,FUNCTION,
                                                 KC_ENT ,KC_HOME ,        KC_END ,KC_ENT,
-                                                SH_TT  ,KC_APP  ,        _______,_______
+                                                _______,KC_APP  ,        _______,_______
   ),
 
   [_FUNCTION] = LAYOUT_5x6(
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                         KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
         _______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,                         _______,_______,_______,_______,_______,_______,
-        _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,                         _______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,
+        _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,                         KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,
         _______,_______,_______,_______,_______,_______,                         _______,_______,_______,_______,_______,_______,
                         _______,RESET  ,                                                         _______,_______,
                                                 _______,_______,         _______,_______,
